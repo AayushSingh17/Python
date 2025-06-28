db3f2927bb76da35aa35ab76e5e698ef1c2ad63b
Aayush Kumar Singh <aayushkumarsingh17@gmail.com>
2025-06-28 14:57:04 +0530

    Delete Python/Aayush singh directory

--- Python/Aayush singh/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-data = {'name': 'Aayush', 'Age': 19}
-# json_str = json.dumps(data) #convert dict to json..
-# print(json_str)
-
-# json_data = {'name': 'Aayush', 'Age': 19}
-# json_str = json.loads(json_data) #convert json to dict...
-# print(data["name"]) #Output : Aayush
